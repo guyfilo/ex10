e8155fb82c3b2debccf04029d44ac463b9526840
guyfilo <59502073+guyfilo@users.noreply.github.com>
2020-01-04 13:06:13 +0200

    Update ship.py

--- ship.py
+++ ship.py
@@ -5,3 +5,20 @@ class Ship:
         self.__x_speed = x_speed
         self.__y_speed = y_speed
         self.__angle = angle
+
+    def get_place(self):
+        return (self.__x_place, self.__y_place)
+
+    def get_speed(self):
+        return (self.__y_speed, self.__y_speed)
+
+    def set_place(self, place):
+        self.__x_place = place[0]
+        self.__y_place = place[1]
+
+    def set_speed(self, speed):
+        self.__x_place = speed[0]
+        self.__y_place = speed[1]
+       
+    def set_angle(self, angle):
+        self.__angle = angle
