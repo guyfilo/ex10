b7d6b64a578778125fb0b034f7cb07a5d2d388ba
guydoren <59502096+guydoren@users.noreply.github.com>
2020-01-04 23:16:02 +0200

    Update asteroid.py

--- asteroid.py
+++ asteroid.py
@@ -1,26 +1,33 @@
 class Asteroid:
     def __init__(self, x_place, y_place, x_speed, y_speed, size):
-        self.__place = (x_place, y_place)
-        self.__speed = (x_speed, y_speed)
+        self.__x_place = x_place
+        self.__y_place = y_place
+        self.__x_speed = x_speed
+        self.__y_speed = y_speed
         self.__size = size
 
     def get_place(self):
-        return self.__place
+        return self.__x_place, self.__y_place
 
     def get_speed(self):
-        return self.__speed
+        return self.__x_speed, self.__y_speed
 
     def set_place(self, place):
-        self.__place = place
+        self.__x_place = place[0]
+        self.__y_place = place[1]
 
     def set_speed(self, speed):
-        self.__speed = speed
+        self.__x_speed = speed[0]
+        self.__y_speed = speed[1]
 
     def has_intersection_with(self, obj):
-        obj_plc = obj.get_place()
-        distance = ((obj_plc[0] - self.__place[0])**2
-                    + (obj_plc[1] - self.__place[1])**2) ** 0.5
+        obj_place = obj.get_place()
+        distance = ((obj_place[0] - self.__x_place)**2
+                    + (obj_place[1] - self.__y_place)**2) ** 0.5
         return distance <= self.__size + len(obj)
 
     def __len__(self):
         return self.__size*10 - 5
+
+    def __str__(self):
+        return str(self.__x_place) + ' ' + str(self.__y_place)
