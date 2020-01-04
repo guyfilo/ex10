f848a602e38a74bcb52e602e7e2dbe516e22d99a
guydoren <59502096+guydoren@users.noreply.github.com>
2020-01-04 12:41:56 +0200

    Create torpedo.py

---
+++ torpedo.py
@@ -0,0 +1,7 @@
+class Torpedo:
+    def __init__(self, x_place, y_place, x_speed,  y_speed, angle):
+        self.__x_place = x_place
+        self.__y_place = y_place
+        self.__x_speed = x_speed
+        self.__y_speed = y_speed
+        self.__angle = angle
