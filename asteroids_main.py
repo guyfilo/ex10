97194969b74933b83bc31d4351018b241e189ce4
guydoren <59502096+guydoren@users.noreply.github.com>
2020-01-04 13:39:24 +0200

    Update asteroids_main.py

--- asteroids_main.py
+++ asteroids_main.py
@@ -16,7 +16,7 @@ class GameRunner:
         self.__screen_min_y = Screen.SCREEN_MIN_Y
         ship_x = random.randint(self.__screen_min_x, self.__screen_max_x)
         ship_y = random.randint(self.__screen_min_y, self.__screen_max_y)
-        self.__ship = Ship(ship_x, ship_y, 0, 0, 0)
+        self.__ship = Ship(ship_x, ship_y, 0, 0, 0.0)
 
     def run(self):
         self._do_loop()
@@ -32,7 +32,8 @@ class GameRunner:
     def _game_loop(self):
         # TODO: Your code goes here
         ship = self.__ship
-        Screen.draw_ship(ship.get_x_place(), ship.get_y_place(),
+        screen = self.__screen
+        screen.draw_ship(ship.get_x_place(), ship.get_y_place(),
                          ship.get_angle())
 
 
