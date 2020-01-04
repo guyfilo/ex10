953c17cbacc6eb958b02e360e7b6ab137f62c231
guyfilo <59502073+guyfilo@users.noreply.github.com>
2020-01-04 12:43:15 +0200

    Update asteroid.py

--- asteroid.py
+++ asteroid.py
@@ -1,1 +1,7 @@
-
+class Asteroid:
+    def __init__(self, x_place, y_place,x_speed, y_speed, size):
+        self.__x_plce = x_place
+        self.__y_place = y_place
+        self.__x_speed = x_speed
+        self.__y_speed = y_speed
+        self.__size = size
