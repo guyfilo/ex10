48c917de2cffdffaba363abdb702f35ae5734bb9
guydoren <59502096+guydoren@users.noreply.github.com>
2020-01-04 13:07:30 +0200

    Update asteroids_main.py

--- asteroids_main.py
+++ asteroids_main.py
@@ -1,5 +1,7 @@
 from screen import Screen
 import sys
+import random
+from ship import Ship
 
 DEFAULT_ASTEROIDS_NUM = 5
 
@@ -8,11 +10,13 @@ class GameRunner:
 
     def __init__(self, asteroids_amount):
         self.__screen = Screen()
-
         self.__screen_max_x = Screen.SCREEN_MAX_X
         self.__screen_max_y = Screen.SCREEN_MAX_Y
         self.__screen_min_x = Screen.SCREEN_MIN_X
         self.__screen_min_y = Screen.SCREEN_MIN_Y
+        ship_x = random.randint(self.__screen_min_x, self.__screen_max_x)
+        ship_y = random.randint(self.__screen_min_y, self.__screen_max_y)
+        self.__ship = Ship(ship_x, ship_y, 0, 0, 0)
 
     def run(self):
         self._do_loop()
