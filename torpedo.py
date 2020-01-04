7be97650b9708377d2fc813d9b01573bc715e08f
guydoren <59502096+guydoren@users.noreply.github.com>
2020-01-04 21:22:50 +0200

    Update torpedo.py

--- torpedo.py
+++ torpedo.py
@@ -1,7 +1,12 @@
 class Torpedo:
+    __size = 5
+
     def __init__(self, x_place, y_place, x_speed,  y_speed, angle):
         self.__x_place = x_place
         self.__y_place = y_place
         self.__x_speed = x_speed
         self.__y_speed = y_speed
         self.__angle = angle
+
+    def __len__(self):
+        return self.__size
