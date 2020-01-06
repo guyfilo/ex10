3e769ed103c7bbfe0eadcf84444baf241741e13f
guydoren <59502096+guydoren@users.noreply.github.com>
2020-01-06 16:05:54 +0200

    Update torpedo.py

--- torpedo.py
+++ torpedo.py
@@ -8,5 +8,17 @@ class Torpedo:
         self.__y_speed = y_speed
         self.__angle = angle
 
+    def get_place(self):
+        return self.__x_place, self.__y_place
+
+    def get_angle(self):
+        return self.__angle
+
+    def get_speed(self):
+        return self.__x_speed, self.__y_speed
+
+    def set_place(self, place):
+        self.__x_place, self.__y_place = place
+
     def __len__(self):
         return self.__size
